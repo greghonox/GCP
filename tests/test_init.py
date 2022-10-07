45a2447dd28dfc070cfbd82d1eff202e86ca3825
gregorio honorato <gregoriohonorato@AZ028.home>
2022-10-07 18:27:43 -0300

    add test in action github

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,7 +1,7 @@
 from os import environ
 
-def test_echo_security_github_test() -> None:
+def test_echo_security_github_test() -> str:
     """ Test print """
     variable = environ.get('SECURITY_GITHUB')
-    print(variable)
+    return variable
     
